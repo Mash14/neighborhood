e3e370d6553b5227b2a192f2e5b98a52b660e280
mash <mashalonzo741@gmail.com>
2021-10-28 16:55:48 +0300

    Add the logout

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hood.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/',views.LogoutView.as_view(), {"next_page": '/'}),
 ]
