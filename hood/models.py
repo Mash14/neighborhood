528cca4437601991a585f13b574e6e2c6bdc3e40
mash <mashalonzo741@gmail.com>
2021-10-29 04:40:12 +0300

    Add neighborhood methods

--- hood/models.py
+++ hood/models.py
@@ -9,4 +9,20 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length=40)
     occupant_count = models.IntegerField()
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,to_field='username' ,on_delete=models.CASCADE,editable=False,null=True)
+    
+    @classmethod
+    def delete_neighborhood(cls,id):
+        cls.objects.filter(id = id).delete()
+
+    @classmethod
+    def get_neighborhood_by_id(cls,id):
+        neighborhoods = cls.objects.get(id = id)
+        return neighborhoods
+
+    @classmethod
+    def update_neighborhood(cls,id,new_name):
+        cls.objects.filter(id = id).update(name = new_name)
+
+    @classmethod
+    def update_occupant_count(cls,id,new_count):
+        cls.objects.filter(id=id).update(occupant_count = new_count)
