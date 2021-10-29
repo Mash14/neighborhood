2bcb1a8af1042409dc217f655ba307add6fcd7cc
mash <mashalonzo741@gmail.com>
2021-10-29 04:24:15 +0300

    Create neighborhood model

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Neighborhood
 
 # Register your models here.
+
+admin.site.register(Neighborhood)
--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    occupant_count = models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,to_field='username' ,on_delete=models.CASCADE,editable=False,null=True)
