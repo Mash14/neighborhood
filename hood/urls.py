530958a378847e0495768415de6a76abcb1e8869
mash <mashalonzo741@gmail.com>
2021-10-30 18:00:02 +0300

    Add route to post neighborhood

--- hood/urls.py
+++ hood/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home_page, name = 'home'),
+    url('post/neighborhood/',views.post_neighborhood,name = 'post_neighborhood'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Neighborhood,Post,UserProfile
+from .forms import NeighborhoodForm
 
 # Create your views here.
 
@@ -13,3 +14,16 @@ def home_page(request):
 
 @login_required(login_url='/accounts/login')
 def post_neighborhood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NeighborhoodForm(request.POST,request.FILES)
+        if form.is_valid():
+            neighborhood = form.save(commit = False)
+            neighborhood.user = current_user
+            neighborhood.save()
+        return redirect('home')
+    else:
+        form = NeighborhoodForm()
+
+    title = 'Upload Neighborhood'
+    return render(request, 'post_neighborhood.html',{'form':form,'title':title})
