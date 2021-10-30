8dd5ddde3f84f4fc8ef3f7b773fb18df6ee8f968
mash <mashalonzo741@gmail.com>
2021-10-30 18:19:17 +0300

    Create profile form

--- hood/forms.py
+++ hood/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Neighborhood
+from .models import Neighborhood,UserProfile
 
 class NeighborhoodForm(forms.ModelForm):
     class Meta:
         model = Neighborhood
         exclude = ['admin'] 
+
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ['user']
