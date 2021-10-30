a97b7412cc9fe308b926a98d8d519469b57dcbb0
mash <mashalonzo741@gmail.com>
2021-10-30 18:24:47 +0300

    Add route to update profile page

--- hood/urls.py
+++ hood/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url('^$',views.home_page, name = 'home'),
     url('post/neighborhood/',views.post_neighborhood,name = 'post_neighborhood'),
     url('profile/',views.profile, name='profile_page'),
+    url('profiles/update/', views.update_profile, name='update_profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Neighborhood,Post,UserProfile
-from .forms import NeighborhoodForm
+from .forms import NeighborhoodForm,NewProfileForm
 
 # Create your views here.
 
@@ -35,3 +35,22 @@ def profile(request):
     title = 'Profile Page'
     return render(request, 'profile.html', {'userProfile':userProfile,'title':title})
 
+
+@login_required(login_url='/accounts/login')
+def update_profile(request):
+    current_user = request.user
+    userProfile = UserProfile.objects.filter(user = current_user).first()
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.user = current_user
+            profile.save()
+            return redirect('profile_page')
+
+    else:
+        form = NewProfileForm()
+        
+    title = 'Update Profile'
+    return render(request, 'update_profile.html',{'form':form,'title':title,'userProfile':userProfile})
