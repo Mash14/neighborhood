53247cb4034bdde7c97a0b3e2004f86e3c0e370e
mash <mashalonzo741@gmail.com>
2021-10-30 11:22:51 +0300

    Add route to home page

--- hood/views.py
+++ hood/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Neighborhood,Post,UserProfile
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
+def home_page(request):
+    posts = Post.objects.order_by('-title')
+
+    title = 'Home'
+    return render(request, 'index.html', {'posts':posts,'title':title})
